afffa2545829f5aa3196ed0219670ef15a82b5a3
Jared Weiss <jared@jaredweiss.net>
2015-01-07 15:40:59 -0500

    updated setup.bash

--- setup.bash
+++ setup.bash
@@ -8,5 +8,8 @@ export GDL_OBJECT_PATH=$GDL_MODEL_PATH/big_bird_models_processed
 export GRASPIT=$GDL_PATH/graspit_gdl
 
 export GDL_GRASPS_PATH=$GDL_PATH/data/grasps
+export GDL_GRASPS_TEMP_PATH=/tmp/grasps
 
 export CGDB_MODEL_ROOT=$GRASPIT/cgdb
+
+export PYLEARN2_DATA_PATH=$GDL_PATH/data/
